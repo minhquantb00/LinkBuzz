619b8ed3fb2064095b21d7d164f3f03ad31ffb42
longndhe176282 <longndhe176282@fpt.edu.vn>
2024-07-25 15:40:16 +0700

    update

---
+++ LinkBuzz.Application/Kafka/Producers/IEventProducer.cs
@@ -0,0 +1,14 @@
+﻿using LinkBuzz.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkBuzz.Application.Kafka.Producers
+{
+    public interface IEventProducer
+    {
+        Task ProducerAsync<T>(string topic, T @event) where T : BaseEvent;
+    }
+}
